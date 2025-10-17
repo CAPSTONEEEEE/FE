d5b406f82351fcb73cf917458aad2a9af787ea0c
yoongabin <yoongabin21@ewhain.net>
2025-10-17 21:29:04 +0900

    챗봇 관련 js 추가

--- sosohaeng-app/src/config/client.js
+++ sosohaeng-app/src/config/client.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // 로컬 테스트 시에는 'http://127.0.0.1:8000'을 사용합니다.
 // Expo Go 앱에서는 컴퓨터의 로컬 IP 주소를 사용해야 합니다.
 // 예: 'http://192.168.0.5:8000'
-const BASE_URL = 'http://192.168.0.36:8000/api/v1'; // 반드시 본인의 IP 주소로 변경하세요.
+const BASE_URL = 'http://{host}:8000/api/v1'; // 반드시 본인의 IP 주소로 변경하세요.
 
 // Axios 인스턴스 생성
 const apiClient = axios.create({
